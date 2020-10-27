fbf2ee85992be2e33c58330ce284b12de4c2e6cf
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-10-27 17:11:06 +0500

    Total expense added.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const addBtn = document.querySelector('#btn-add');
 const clearBtn = document.querySelector('#btn-clear');
 
 const expenseList = document.querySelector('#expenses-list');
+const totalExpense = document.querySelector('#total-expense');
+
+let total = 0;
 
 const clear = () => {
     reasonInput.value = '';
@@ -19,9 +22,13 @@ addBtn.addEventListener('click', () => {
         return;
 
     const newItem = document.createElement('ion-item');
-    newItem.textContent = enteredReason + ': $'+ enteredAmount;
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    total += +enteredAmount;
 
     expenseList.appendChild(newItem);
+
+    totalExpense.textContent = total;
 
     clear();
 });
