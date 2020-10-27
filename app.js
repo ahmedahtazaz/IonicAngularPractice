224eee8b2dcb0cd516140bb143255bad8ea766d4
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-10-27 16:35:08 +0500

    App.js file added and used.

---
+++ app.js
@@ -0,0 +1,13 @@
+const reasonInput = document.querySelector('#reason');
+const amountInput = document.getElementById('#amount');
+
+const addBtn = document.querySelector('#btn-add');
+const clearBtn = document.querySelector('#btn-clear');
+
+addBtn.addEventListener('click', () => {
+    console.log('Add btn press');
+});
+
+clearBtn.addEventListener('click', () => {
+    console.log('Clear btn press');
+});
